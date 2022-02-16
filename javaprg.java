f9b1ec31ff32a3c246122cc8c1d4018790e1cfa0
vim <vimran@>
2022-02-16 18:01:49 +0000

    updated java program

--- javaprg.java
+++ javaprg.java
@@ -1,5 +1,10 @@
 class Simple{  
+public void Hello(){
+System.out.println("This will print message as hello")
+}
+
     public static void main(String args[]){  
      System.out.println("Hello Java");  
     }  
+
 }  
