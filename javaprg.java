7e9671575115798a5652a2ba8ee167f945443a8c
vim <vimran@>
2022-02-16 18:06:02 +0000

    updated the java program with world method

--- javaprg.java
+++ javaprg.java
@@ -1,4 +1,7 @@
 class Simple{  
+public void world (){
+System.out.println("This will print the output as world");
+}
     public static void main(String args[]){  
      System.out.println("Hello Java");  
     }  
